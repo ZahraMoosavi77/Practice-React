7565a3120f0c9f3b437bfa9fc028682697b799ce
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-06-20 20:21:47 +0330

    fix App component

--- countChar/countChar/src/App.jsx
+++ countChar/countChar/src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
-
+import Text from './component/Text'
 import FollowerInput from './component/FollowerInput'
+
 function App() {
   
 
   return (
    <div>
       <FollowerInput/>
+     
+
    </div>
   )
 }
