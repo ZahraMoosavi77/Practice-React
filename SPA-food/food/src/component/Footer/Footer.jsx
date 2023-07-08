07226e91029ae095b639cf72535beff7139348bc
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-07-08 17:41:26 +0330

    add footer component

--- SPA-food/food/src/component/Footer/Footer.jsx
+++ SPA-food/food/src/component/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import './Footer.css'
 export function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className="footer__icons">
+        <img className='footer--icon' src='linkedin.svg' alt="" />
+        <img className='footer--icon' src='.svg' alt="" />
+        <img className='footer--icon' src='linkedin.svg' alt="" />
+        <img className='footer--icon' src='linkedin.svg' alt="" />
+      </div>
+      <div className="footer__text">
+        <p>@2023 Daneshkarfood.com</p>
+      </div>
+
+    </footer>
   )
 }
