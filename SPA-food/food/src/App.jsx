81ff228c6a5f754f36d8dc747e2cf586888b3d25
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-07-08 17:40:04 +0330

    add home page

--- SPA-food/food/src/App.jsx
+++ SPA-food/food/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import {Header, Footer} from "./export"
+import {Header, Footer, Home} from "./export"
+
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
+    <div>
       
       <Header/>
+      <Home/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
