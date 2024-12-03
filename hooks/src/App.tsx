cbf5b03b279ce552a0b9d6adf7bbc959572caa0b
12345 <moosavi_z@cyberno.ir>
2024-12-03 09:33:36 +0330

    add :  add learn reducer

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,29 +1,11 @@
-import { useEffect, useRef, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  // const renderCount = useRef(1);
-  // const [renderCount, setRenderCount] = useState(0)
-  // useEffect(()=>{
-  //   setRenderCount(prevREnderCount => prevREnderCount + 1)
-  // })
 
-  // useEffect(() => {
-  //   renderCount.current = renderCount.current + 1;
-  // });
-  // const inputRef:any = useRef()
-  const prevName:any = useRef()
-  useEffect (()=>{
-    prevName.current = name
-  },[name])
   return (
     <>
-      {/* ref={inputRef} */}
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      {/* <div>I rendered {renderCount.current} times</div> */}
-      <div> my name is {name} and it used to be {prevName.current}</div>
+        
     </>
   );
 }
---
+++ hooks/src/components/LearnRef.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+const LearnRef = ()=>{
+    const [name, setName] = useState("");
+    // const renderCount = useRef(1);
+    // const [renderCount, setRenderCount] = useState(0)
+    // useEffect(()=>{
+    //   setRenderCount(prevREnderCount => prevREnderCount + 1)
+    // })
+  
+    // useEffect(() => {
+    //   renderCount.current = renderCount.current + 1;
+    // });
+    // const inputRef:any = useRef()
+    const prevName:any = useRef()
+    useEffect (()=>{
+      prevName.current = name
+    },[name])
+    return (
+      <>
+        {/* ref={inputRef} */}
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        {/* <div>I rendered {renderCount.current} times</div> */}
+        <div> my name is {name} and it used to be {prevName.current}</div>
+      </>
+    );
+}
